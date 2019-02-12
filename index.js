0e31363c348a191176706ac67e0a6271f297920c
Litterial <yungice12@gmail.com>
2019-02-12 15:25:45 -0600

    renamed some classes. working on projects

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-$('div').css('background-color','green');
-$('section').css('border','1px solid black');
+
+$('.container').css('border','1px solid black');
 $(".light").css('background-color','blue');
 $('.dark').css('background-color','red');
 
