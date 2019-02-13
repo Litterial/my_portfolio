5cb1b46f7a6f2fd21e38c576fe90899ede7dbb19
Litterial <yungice12@gmail.com>
2019-02-13 15:07:51 -0600

    Early stages of grid layout on audio logs

---
+++ index.js
@@ -0,0 +1,7 @@
+var audioLogAlpha=document.getElementById('Alpha').controls;
+var audioLog1=document.getElementById('Week1').controls;
+var audioLog2=document.getElementById('Week2').controls;
+var audioLog3=document.getElementById('Week3').controls;
+var audioLog4=document.getElementById('Week4').controls;
+/*
+document.getElementById('Week1').innerText=audioLogs;*/
