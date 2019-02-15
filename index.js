be4af3224e5935987d9476a161d125031b926512
Litterial <yungice12@gmail.com>
2019-02-15 12:05:19 -0600

    Finished arrows. Updating Firefox

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ var audioLog1=document.getElementById('Week1').controls;
 var audioLog2=document.getElementById('Week2').controls;
 var audioLog3=document.getElementById('Week3').controls;
 var audioLog4=document.getElementById('Week4').controls;
+var arrow=document.getElementById('scrollUp');
+arrow.addEventListener('click',removeBorder);
+
+function removeBorder()
+{
+arrow.blur();
+}
 /*
 document.getElementById('Week1').innerText=audioLogs;*/
