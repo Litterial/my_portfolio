d03c0cf5dd46e213b0b9b5a81075154bffa554a9
Litterial <yungice12@gmail.com>
2020-05-18 23:29:16 -0500

    hamburger menu for navigation bar. navigtaion bar covers full screen on mobile display

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ $(document).scroll(function()
     var x=$(this).scrollTop();
     if (x > 100) {
         $('#scrollUp').css({'display':'block'}).addClass('fadeInRight animated').removeClass('fadeOutRight ');
-        console.log(x)
     }
    else
     {
@@ -11,3 +10,30 @@ $(document).scroll(function()
         $('#scrollUp').removeClass('fadeInRight animated').addClass('fadeOutRight animated');
     }
 });
+
+
+     
+     var links = document.querySelectorAll('#navbar div a'); 
+     let checkbox = document.getElementById('hamburger');
+
+     if (window.innerWidth <=767) {
+        for( let i=0; i<links.length; i++){
+            links[i].addEventListener("click",menu);
+        }
+     }
+
+     else{
+        for( let i=0; i<links.length; i++){
+            links[i].removeEventListener("click",menu);
+        }
+     }
+
+     function menu (e) {
+        checkbox.checked ==true? checkbox.checked = false: checkbox.checked=true;
+    }
+         
+
+    
+
+   
+   
