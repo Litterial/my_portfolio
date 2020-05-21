9fd94e0c7585a4704fe554d8a44c1751045e5dc9
Litterial <yungice12@gmail.com>
2020-05-20 23:01:59 -0500

    navbar event listeners work on mobile. Responsive header

--- index.js
+++ index.js
@@ -12,28 +12,34 @@ $(document).scroll(function()
 });
 
 
-     
-     var links = document.querySelectorAll('#navbar div a'); 
+
+     let media = window.matchMedia("(max-width:767px)");
+     let links = document.querySelectorAll('#navbar div a'); 
      let checkbox = document.getElementById('hamburger');
+   
 
-     if (window.innerWidth <=767) {
+function menu (e)
+{  
+    if (e.matches) {
         for( let i=0; i<links.length; i++){
-            links[i].addEventListener("click",menu);
+            links[i].addEventListener("click",toggleMenu);
         }
-     }
-
-     else{
-        for( let i=0; i<links.length; i++){
-            links[i].removeEventListener("click",menu);
-        }
-     }
-
-     function menu (e) {
-        checkbox.checked ==true? checkbox.checked = false: checkbox.checked=true;
     }
+    else{
+        for( let i=0; i<links.length; i++){
+            links[i].removeEventListener("click",toggleMenu);
+        }
+    }
+}
+
+function toggleMenu (e) {
+         console.log("clicked");
+        checkbox.checked ==true? checkbox.checked = false: checkbox.checked=true;
+}
          
 
     
-
+     menu(media);
+     media.addListener(menu);
    
    
